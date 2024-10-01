bedff1ba452592db2a88111879f6b4c04c9466d6
[maggiieee1] <[margarita.lizardo@wvsu.edu.ph]>
2024-10-01 18:37:24 +0800

    save changes made

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,4 @@
-// This is a basic Flutter widget test.
+//This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
@@ -13,7 +13,7 @@ import 'package:cc206_emoti_sense/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
